ac7b0a3636bbabed6b79a89b4b7c00023face9f8
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-10-15 08:16:03 -0400

    sdf

---
+++ src/FourForLoops.java
@@ -0,0 +1,19 @@
+public class FourForLoops {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for(int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for(int i = 0; i <= 18; i+=3) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for(int i = 10; i >= 0; i-=2) {
+            System.out.print(i + " ");
+        }
+    }
+}
